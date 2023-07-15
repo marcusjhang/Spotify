176e65aa269b9bd8bc679d9605b022182ce3d8a1
Marcus Ang <marcusajh0802@gmail.com>
2023-07-15 11:39:05 +0800

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, InputGroup, FormControl, Button, Row, Card } from 'react-bootstrap';
+import { useState, useEffect } from 'react' 
+const CLIENT_ID = process.env.CLIENT_ID
+const CLIENT_SECRET = process.env.CLIENT_SECRET
 
 function App() {
+  const [searchInput, setSearchInput] = useState('');
+  const [accessToken, setAccessToken] = useState('');
+  const [albums, setAlbums] = useState([]);
+
+
+  useEffect(() => {
+    //API Access Token
+    var authParameters = {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+    }
+    fetch('https://accounts.spotify.com/api/token', authParameters)
+      .then(result => result.json())
+      .then(data => setAccessToken(data.access_token))
+  }, [])
+
+  async function search() {
+    console.log("Search for " + searchInput)
+
+    var searchParameters = {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer '+ accessToken
+      }
+    }
+    var artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist', searchParameters)
+    .then(response => response.json())
+    .then(data => {return data.artists.items[0].id})
+
+    var albums = await fetch('https://api.spotify.com/v1/artists/' + artistID +'/albums' + '?include_groups=album&market=US&limit=50', searchParameters)
+    .then(response => response.json())
+    .then(data => {
+      setAlbums(data.items);
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="fw-bold my-3">Spotify Search</h1>
+      <Container>
+        <InputGroup className="mb-3" size="lg">
+          <FormControl 
+            placeholder="Search For Artist"
+            type="input"
+            onChange={event => setSearchInput(event.target.value)}
+          />
+          <Button onClick={search}>
+            Search
+          </Button>
+        </InputGroup>
+      </Container>
+      <Container>
+        <Row className="mx-2 row row-cols-4">
+          {albums.map((album, index) => {
+            console.log(album)
+            return (
+              <Card>
+              <Card.Img src={album.images[0].url} />
+              <Card.Body>
+                <Card.Title>{album.name}</Card.Title>
+              </Card.Body>
+            </Card>
+            )
+          })}
+        </Row> 
+      </Container>
     </div>
   );
 }
